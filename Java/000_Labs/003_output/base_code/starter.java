72af78fab8f483b2fc2a300b7be17f98d6b47f48
ArmenPC <goodjobarmen@gmail.com>
2026-09-03 22:19:05 +0000

    update

--- Java/000_Labs/003_output/base_code/starter.java
+++ Java/000_Labs/003_output/base_code/starter.java
@@ -9,6 +9,15 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+	
+
+
+	    System.out.println ("Hello!");
+        System.out.println ("This is Armen here in this tutorial for simple coding.");
+
+		System.out.println ("Happy birthday, to you!");
+	    System.out.println ("Happy birthday, to you!");
+		System.out.println ("Happy birthday, dear David!");
+		System.out.println ("Happy birthday, to you!");
 	}
 }
